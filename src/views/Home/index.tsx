5e685f6d65202210029f754d3be2bdcc14b84768
Achthar <achim.huebl@gmail.com>
2022-07-20 01:33:15 +0200

    write front page with stepper

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -16,13 +16,28 @@ const Home: React.FC = () => {
   const { isMobile } = useMatchBreakpoints()
 
 
-  const intro = 'The Requiem Finance Protocol is governed by the Governance Requiem Token (GREQ). That token can be minted when locking abREQ. At max, one GREQ can be minted for one abREQ where the exact amount you obtain depends on the amount of time you decide to lock abREQ. abREQ can be withdrawn early, but with a hefty penalty.'
+  const intro = `The Requiem Finance Protocol is governed by the Governance Requiem Token (GREQ). 
+  That token can be minted when locking abREQ. 
+  At max, one GREQ can be minted for one abREQ where the exact amount you obtain depends on the amount of time you decide to lock abREQ. 
+  abREQ can be withdrawn early, but with a hefty penalty.`
 
-  const outlook = 'The penalties collected will partly be burnt and also used to redistribute for yield farming to increase engagement on the platform without inflating abREQ itself.'
- 
-  const locking = 'We allow the creating of multiple locks/locked positions so that you can flexibly decide on which mouynt to lock for a specific amount of time. As explained above, the amount of time abREQ is locked determines the amount of GREQ minted, where more specifically, the amount minted increases exponentially the longer you decide to lock it.'
-  
-  const status: Status[] = ['past', 'past', 'past', 'past', 'past', 'current', 'future']
+  const outlook = `The penalties collected will partly be burnt and also used to redistribute for yield farming to increase engagement on the platform without inflating abREQ itself.`
+
+  const locking = `We allow the creating of multiple locks/locked positions so that you can flexibly decide on which mouynt to lock for a specific amount of time. 
+  As explained above, the amount of time abREQ is locked determines the amount of GREQ minted, where more specifically, 
+  the amount minted increases exponentially the longer you decide to lock it.`
+
+  const status: Status[] = ['past', 'past', 'past', 'future']
+
+  const headers = ['Get abREQ', 'Lock abREQ for GREQ', 'Stake GREQ and Earn', 'Propose and Vote!']
+
+  const steps = [
+    'You can get abREQ on our exchange or by selling assets to our treasury using bonds',
+    'Lock abREQ for some time to mint GREQ. Be careful with considerng for how long you wan tto lock as there is a penalty on early withdrawls.',
+    'Stake GREQ to earn rewards paid out in valuable assets such as USDC or WETH.',
+    'Maximize and use your voting power to impact the direction the Requiem Finance Protocol will go.'
+  ]
+
 
   const titleFont = {
     lineHeight: 1.2,
@@ -37,7 +52,7 @@ const Home: React.FC = () => {
     marginTop: 15,
   }
 
-  const iconStyle = isMobile ? mobileIconStyle : {}
+  const headerColor = 'rgba(255, 40, 40, 0.5)';
 
   return (
     <>
@@ -48,7 +63,7 @@ const Home: React.FC = () => {
         index={2}
         hasCurvedDivider={false}
       >
-        <Container width="100%" maxWidth="25000px" style={{ marginBottom: 60 }}>
+        <Container width="100%" maxWidth="25000px" style={{ marginBottom: 10 }}>
           <div
             style={{
               justifyContent: 'center'
@@ -63,53 +78,33 @@ const Home: React.FC = () => {
           <Text fontSize={isMobile ? "14px" : "24px"} color='white' marginBottom='10px'>
             {intro}
           </Text>
-
           <Text fontSize={isMobile ? "14px" : "24px"} color='white' marginTop='10px'>
             {outlook}
           </Text>
           <Text fontSize={isMobile ? "14px" : "24px"} color='white' marginTop='10px'>
             {locking}
           </Text>
+          <Text fontSize={isMobile ? "14px" : "24px"} color='rgba(255, 40, 40, 0.75)' marginTop='10px'>
+            The following steps are necessary to earn from the Requiem Protocol:
+          </Text>
         </Container>
 
-        {/* <ReactMarkdown children={getText()} /> */}
-
-        {/*
-      <PageSection
-        innerProps={{ style: HomeSectionContainerStyles }}
-        background={theme.colors.background}
-        index={2}
-        hasCurvedDivider={false}
-      >
-        <OuterWedgeWrapper>
-          <InnerWedgeWrapper top fill={theme.isDark ? '#201335' : '#D8CBED'}>
-            <WedgeTopLeft />
-          </InnerWedgeWrapper>
-        </OuterWedgeWrapper>
-        <SalesSection {...swapSectionData} />
-      </PageSection>
-      <PageSection
-        innerProps={{ style: HomeSectionContainerStyles }}
-        index={2}
-        hasCurvedDivider={false}
-      >
-        <OuterWedgeWrapper>
-          <InnerWedgeWrapper width="150%" top fill={theme.colors.background}>
-            <WedgeTopRight />
-          </InnerWedgeWrapper>
-        </OuterWedgeWrapper>
-        <SalesSection {...earnSectionData} />
-        <FarmsPoolsRow {...chainId} />
-      </PageSection>
-      <PageSection
-        innerProps={{ style: HomeSectionContainerStyles }}
-        background={theme.colors.background}
-        index={2}
-        hasCurvedDivider={false}
-      >
-        <SalesSection {...REQTSectionData} />
-        <REQTDataRow />
-      </PageSection> */}
+        <Stepper>
+          {steps.map((step, index) => (
+            <Step key={step} index={index} status={status[index]}>
+              <Card>
+                <CardBody>
+                  <Text fontSize="24px" textTransform="capitalize" bold color={headerColor}>
+                    {headers[index]}
+                  </Text>
+                  <Text>
+                    {step}
+                  </Text>
+                </CardBody>
+              </Card>
+            </Step>
+          ))}
+        </Stepper>
       </PageSection>
     </>
   )
